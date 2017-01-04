e178e5eaa2a9feaea07e6d4dbec68348b0903999
Jon Hunter <jhunter@altc02lv2jgfd58.home>
2017-01-04 21:52:59 +0000

    fixed multiple of one discount test

--- test/offerservice-tests.cs
+++ test/offerservice-tests.cs
@@ -10,14 +10,23 @@ namespace Checkout.Tests
         private OfferService _offerService;
 
         [Test]
+        public void If_There_Are_No_Items_To_Calculate_The_Discount_Is_0()
+        {
+            _offerService = new OfferService();
+            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem>());
+
+            Assert.AreEqual(0, discountAmount);
+        }
+
+        [Test]
         public void If_I_Have_3_Of_A_The_Discount_Is_20()
         {
-            _offerService = new OfferService(new List<IItem> {
+            _offerService = new OfferService();
+            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("A"),
                 new Item("A"),
                 new Item("A")
             });
-            var discountAmount = _offerService.CalculateDiscountFromOffers();
 
             Assert.AreEqual(20, discountAmount);
         }
@@ -25,11 +34,11 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_2_Of_B_The_Discount_Is_15()
         {
-            _offerService = new OfferService(new List<IItem> {
+            _offerService = new OfferService();
+            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("B"),
                 new Item("B")
             });
-            var discountAmount = _offerService.CalculateDiscountFromOffers();
 
             Assert.AreEqual(15, discountAmount);
         }
@@ -37,14 +46,14 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_3_Of_A_And_2_Of_B_The_Discount_Is_35()
         {
-            _offerService = new OfferService(new List<IItem> {
+            _offerService = new OfferService();
+            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("A"),
                 new Item("A"),
                 new Item("A"),
                 new Item("B"),
                 new Item("B")
             });
-            var discountAmount = _offerService.CalculateDiscountFromOffers();
 
             Assert.AreEqual(35, discountAmount);
         }
@@ -52,7 +61,8 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_6_Of_A_The_Discount_Is_40()
         {
-            _offerService = new OfferService(new List<IItem> {
+            _offerService = new OfferService();
+            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("A"),
                 new Item("A"),
                 new Item("A"),
@@ -60,7 +70,6 @@ namespace Checkout.Tests
                 new Item("A"),
                 new Item("A")
             });
-            var discountAmount = _offerService.CalculateDiscountFromOffers();
 
             Assert.AreEqual(40, discountAmount);
         }
