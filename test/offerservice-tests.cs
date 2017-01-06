a6d7d5ffcde00e814257be51bb460ed58ab3e4fe
Jon Hunter <jhunter@ALTC02LV2JGFD58.local>
2017-01-06 08:39:26 +0000

    refactored offer service and rearranged structure

--- test/offerservice-tests.cs
+++ test/offerservice-tests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using Checkout.Interfaces;
+using Checkout.Offers;
 
 namespace Checkout.Tests
 {
@@ -12,7 +13,7 @@ namespace Checkout.Tests
         [Test]
         public void If_There_Are_No_Items_To_Calculate_The_Discount_Is_0()
         {
-            _offerService = new OfferService();
+            _offerService = new OfferService(new List<IOffer>());
             var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem>());
 
             Assert.AreEqual(0, discountAmount);
@@ -21,8 +22,11 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_3_Of_A_The_Discount_Is_20()
         {
-            _offerService = new OfferService();
-            var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
+            _offerService = new OfferService(new List<IOffer> {
+                new QuantityDiscountOffer("A", 3, 20)
+            });
+            var discountAmount = _offerService.CalculateDiscountFromOffers(
+                new List<IItem> {
                 new Item("A"),
                 new Item("A"),
                 new Item("A")
@@ -34,7 +38,9 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_2_Of_B_The_Discount_Is_15()
         {
-            _offerService = new OfferService();
+            _offerService = new OfferService(new List<IOffer> {
+                new QuantityDiscountOffer("B", 2, 15)
+            });
             var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("B"),
                 new Item("B")
@@ -46,7 +52,10 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_3_Of_A_And_2_Of_B_The_Discount_Is_35()
         {
-            _offerService = new OfferService();
+            _offerService = new OfferService(new List<IOffer> {
+                new QuantityDiscountOffer("A", 3, 20),
+                new QuantityDiscountOffer("B", 2, 15)
+            });
             var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("A"),
                 new Item("A"),
@@ -61,7 +70,9 @@ namespace Checkout.Tests
         [Test]
         public void If_I_Have_6_Of_A_The_Discount_Is_40()
         {
-            _offerService = new OfferService();
+            _offerService = new OfferService(new List<IOffer> {
+                new QuantityDiscountOffer("A", 3, 20)
+            });
             var discountAmount = _offerService.CalculateDiscountFromOffers(new List<IItem> {
                 new Item("A"),
                 new Item("A"),
